5577ba7bad6adce1c9a2ea950c1d422887d76b4a
Faixabet <faixaricaa@gmail.com>
2025-12-22 03:07:32 -0300

    fix .app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,17 @@
-# streamlit_app.py 21/12/2025   
-
+# streamlit_app.py
 
 import sys
 import os
 import streamlit as st
-sys.path.append(os.path.join(os.path.dirname(__file__), "app"))
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+APP_DIR = os.path.join(BASE_DIR, "app")
+
+if APP_DIR not in sys.path:
+    sys.path.insert(0, APP_DIR)
 
 st.write("BOOT OK - streamlit_app")
+
+from app.main import main
+
+main()
